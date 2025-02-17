faaf4d02e984451447340af66e63cee68c7febe2
SankaUdeshika <sankaudeshika123@gmail.com>
2025-02-18 02:07:50 +0530

    Config the Firebase Environment

--- app/src/main/java/lk/sankaudeshika/androidfixers/MainActivity.java
+++ app/src/main/java/lk/sankaudeshika/androidfixers/MainActivity.java
@@ -1,12 +1,18 @@
 package lk.sankaudeshika.androidfixers;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.dynamicanimation.animation.DynamicAnimation;
+import androidx.dynamicanimation.animation.SpringAnimation;
+import androidx.dynamicanimation.animation.SpringForce;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,48 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //        Logo Spring Animation
+        ImageView logoImageView = findViewById(R.id.imageView);
+        SpringAnimation springAnimation = new SpringAnimation(logoImageView, DynamicAnimation.TRANSLATION_Y);
+
+        SpringForce springForce = new SpringForce();
+        springForce.setStiffness(SpringForce.STIFFNESS_VERY_LOW);
+        springForce.setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY);
+        springForce.setFinalPosition(600f);
+
+        springAnimation.setSpring(springForce);
+        springAnimation.start();
+
+
+
+//        Loading PreLoader Page
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+
+        new Thread(new Runnable() {
+
+
+            int x=0;
+            @Override
+            public void run() {
+                for( int i = 0;i<100; i++){
+                    x = i;
+                    try {
+                        Thread.sleep(50);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            progressBar.setProgress(x,true);
+                        }
+                    });
+                }
+                Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        }).start();
     }
 }
