1789e748cf019ebfa6ef1bebe074a93e505f0924
SankaUdeshika <sankaudeshika123@gmail.com>
2025-02-21 14:00:31 +0530

    Done Update Order Status process

--- app/src/main/java/lk/sankaudeshika/androidfixers/model/PendingOrders.java
+++ app/src/main/java/lk/sankaudeshika/androidfixers/model/PendingOrders.java
@@ -1,19 +1,53 @@
 package lk.sankaudeshika.androidfixers.model;
 
 public class PendingOrders {
-    String OrderText ;
+    String customerMobile;
+    String date;
+
+    public PendingOrders(String customerMobile, String date, String time, String bookingID) {
+        this.customerMobile = customerMobile;
+        this.date = date;
+        this.time = time;
+        this.bookingID = bookingID;
+    }
+
+    public String getBookingID() {
+        return bookingID;
+    }
+
+    public void setBookingID(String bookingID) {
+        this.bookingID = bookingID;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustomerMobile() {
+        return customerMobile;
+    }
+
+    public void setCustomerMobile(String customerMobile) {
+        this.customerMobile = customerMobile;
+    }
+
+    String time;
+    String bookingID;
 
     public PendingOrders() {
     }
-    public PendingOrders(String text) {
-        OrderText = text;
-    }
 
-    public String getOrderText() {
-        return OrderText;
-    }
 
-    public void setOrderText(String orderText) {
-        OrderText = orderText;
-    }
 }
--- app/src/main/java/lk/sankaudeshika/androidfixers/ui/pendingorders/PendingOrdersFragment.java
+++ app/src/main/java/lk/sankaudeshika/androidfixers/ui/pendingorders/PendingOrdersFragment.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -22,7 +24,9 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 import lk.sankaudeshika.androidfixers.R;
 import lk.sankaudeshika.androidfixers.databinding.FragmentPendingOrdersBinding;
@@ -56,27 +60,30 @@ public class PendingOrdersFragment extends Fragment {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
 
                         ArrayList<PendingOrders> pendingOrdersList = new ArrayList<>();
-
-
                         List<DocumentSnapshot> list = task.getResult().getDocuments();
 
-//                        Log.i("appout", "adasda: "+vendor_mobile);
-
                         for (DocumentSnapshot documentSnapshot : list) {
-                            // Get your booking details here
+
                             String bookingId = documentSnapshot.getId();
-                            String customerName = documentSnapshot.getString("cusomier_id");
-                            // Handle each booking as needed
-                            Log.i("appout", "adasda: "+customerName);
-                            pendingOrdersList.add(new PendingOrders("asdaiusd"));
+                            String date = documentSnapshot.getString("date");
+                            String time = documentSnapshot.getString("time");
+                            String cutomer_mobile = documentSnapshot.getString("cusomier_id");
+                            pendingOrdersList.add(new PendingOrders(cutomer_mobile,date,time,bookingId));
+
                         }
+
+
+                        for (PendingOrders pendingItem: pendingOrdersList) {
+                            Log.i("appout", "listIS: "+pendingItem.getBookingID());
+                        }
+
 
                         RecyclerView pendingJosBox = view.findViewById(R.id.pendingOrdersRecycleView);
                         pendingJosBox.setLayoutManager(new LinearLayoutManager(view.getContext(), LinearLayoutManager.VERTICAL, false));
                         pendingJosBox.setAdapter(new OrderAdapter(pendingOrdersList, view.getContext()));
+
                     }
                 });
-//        pendingOrdersList.add(new PendingOrders("OK"));
 
 
 
@@ -91,11 +98,21 @@ class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.PendingOrderViewHol
 
 
     class PendingOrderViewHolder extends RecyclerView.ViewHolder{
-        TextView textExample ;
-
+        TextView customerMobile ;
+        TextView customerName;
+        TextView bookingId;
+        TextView date;
+        Button approvedBtn;
+        Button orderCancel;
         public PendingOrderViewHolder(@NonNull View itemView) {
             super(itemView);
-            textExample = itemView.findViewById(R.id.textView21);
+            customerName = itemView.findViewById(R.id.CustomerName);
+            customerMobile = itemView.findViewById(R.id.customerMobile);
+            date = itemView.findViewById(R.id.datetime);
+            bookingId = itemView.findViewById(R.id.bookingID);
+            approvedBtn = itemView.findViewById(R.id.approvedBtn);
+            orderCancel = itemView.findViewById(R.id.orderCancel);
+
         }
     }
     private ArrayList<PendingOrders> pendingOrdersArrayList;
@@ -120,8 +137,48 @@ class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.PendingOrderViewHol
     @Override
     public void onBindViewHolder(@NonNull PendingOrderViewHolder holder, int position) {
         PendingOrders pendingOrdersObject = pendingOrdersArrayList.get(position);
-        Log.i("appout", "onBindViewHolder: " +pendingOrdersArrayList.get(position).getOrderText());
-        holder.textExample.setText(pendingOrdersArrayList.get(position).getOrderText());
+        Log.i("appout", "onBindViewHolder: " +pendingOrdersArrayList.get(position).getBookingID());
+        holder.customerName.setText(pendingOrdersArrayList.get(position).getBookingID());
+        holder.customerMobile.setText(pendingOrdersArrayList.get(position).getCustomerMobile());
+        holder.date.setText(pendingOrdersArrayList.get(position).getDate());
+        holder.bookingId.setText(pendingOrdersArrayList.get(position).getBookingID());
+        holder.approvedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String bookingId = holder.bookingId.getText().toString();
+                Log.i("appout", "onClicked"+bookingId);
+                FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                HashMap<String, Object> updateStatusMap = new HashMap<>();
+                updateStatusMap.put("status","active");
+                firestore.collection("booking").document(bookingId).update(updateStatusMap)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                               RecyclerView recyclerView = view.findViewById(R.id.pendingOrdersRecycleView);
+                                new AlertDialog.Builder(view.getContext()).setTitle("Update Success").setMessage("Order Appoinment is Successfully Active.").show();
+
+                            }
+                        });
+            }
+        });
+
+        holder.orderCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String bookingId = holder.bookingId.getText().toString();
+                FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                HashMap<String, Object> updateStatusMap = new HashMap<>();
+                updateStatusMap.put("status","cancel");
+                firestore.collection("booking").document(bookingId).update(updateStatusMap)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                RecyclerView recyclerView = view.findViewById(R.id.pendingOrdersRecycleView);
+                                new AlertDialog.Builder(view.getContext()).setTitle("Order Canceled").setMessage("Order Appoinment is Successfully Canceled.").show();
+                            }
+                        });
+            }
+        });
     }
 
     @Override
