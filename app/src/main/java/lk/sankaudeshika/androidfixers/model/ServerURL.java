427b1ab2eae70140892d4d76fa3bceef7b977b55
SankaUdeshika <sankaudeshika123@gmail.com>
2025-02-25 11:12:33 +0530

    Done Programme the admin manage Customer

--- app/src/main/java/lk/sankaudeshika/androidfixers/model/ServerURL.java
+++ app/src/main/java/lk/sankaudeshika/androidfixers/model/ServerURL.java
@@ -1,7 +1,7 @@
 package lk.sankaudeshika.androidfixers.model;
 
 public class ServerURL {
-    public static String ServerLink = "https://b494-2402-4000-2100-b61e-cdc-e5ff-fe56-f00d.ngrok-free.app/FixerBeeWebServer/";
+    public static String ServerLink = "https://refined-hare-jolly.ngrok-free.app/FixerBeeWebServer/";
     public static String ServerImages = ServerLink+"images/";
     public static String ServerUrl = ServerLink+"ImageRecieverServlet";
 }
